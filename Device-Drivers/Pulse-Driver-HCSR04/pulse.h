8641fa8c3809852198bf4c1c723bd08eebae134c
Shah Sharvil <spshah16@asu.edu>
2018-05-29 00:15:43 -0700

    Update pulse.h

--- Device-Drivers/Pulse-Driver-HCSR04/pulse.h
+++ Device-Drivers/Pulse-Driver-HCSR04/pulse.h
@@ -1,8 +1,8 @@
 #define DEVICE_NAME "pulse_device"
 #define DRIVER_NAME "pulse_driver"
 
-#define GP_IO2 13  			//GPIO13 corresponds to IO2
-#define GP_IO3 14  			//GPIO14 corresponds to IO3
+#define GP_IO2 13  		//GPIO13 corresponds to IO2
+#define GP_IO3 14  		//GPIO14 corresponds to IO3
 #define GP_IO2_LEV 34  		//GPIO34 corresponds to Level Shifter for IO2
 #define GP_IO3_LEV 16  		//GPIO16 corresponds to Level Shifter for IO3
 
