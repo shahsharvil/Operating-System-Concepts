00d869d3608be70e00c6bd4a36a768eb42170054
Shah Sharvil <spshah16@asu.edu>
2018-06-02 01:06:48 -0700

    Update pulse.h

--- Device-Drivers/Pulse-Driver-HCSR04/pulse.h
+++ Device-Drivers/Pulse-Driver-HCSR04/pulse.h
@@ -1,17 +1,26 @@
+/*
+ * pulse.h - definitions for the char module
+ */
+
+#ifndef _PULSE_H_
+#define _PULSE_H_
+
 #define DEVICE_NAME "pulse_device"
 #define DRIVER_NAME "pulse_driver"
 
-#define GP_IO2 13  		//GPIO13 corresponds to IO2
-#define GP_IO3 14  		//GPIO14 corresponds to IO3
-#define GP_IO2_LEV 34  		//GPIO34 corresponds to Level Shifter for IO2
-#define GP_IO3_LEV 16  		//GPIO16 corresponds to Level Shifter for IO3
+#define GP_IO2 13  			/* GPIO13 corresponds to IO2 */
+#define GP_IO3 14  			/* GPIO14 corresponds to IO3 */
+#define GP_IO2_LEV 34  		/* GPIO34 corresponds to Level Shifter for IO2 */
+#define GP_IO3_LEV 16  		/* GPIO16 corresponds to Level Shifter for IO3 */
 
-struct pulse_device_wrap
+struct pulse_dev
 {
-	struct cdev cdev;
-	char name[25];
-	int irq;
-	unsigned int busy_flag;
-	uint64_t pulse_rising_time;
-	uint64_t pulse_falling_time;
+	struct cdev cdev;				/* char device structure*/
+	char name[25];					
+	int irq;						/* irq line number */
+	unsigned int busy_flag;			/* used to synchronize pulse measurements */
+	uint64_t pulse_rising_time;		/* time stamp of rising pulse */
+	uint64_t pulse_falling_time;	/* time stamp of falling pulse */
 }; 
+
+#endif /* _PULSE_H_ *
