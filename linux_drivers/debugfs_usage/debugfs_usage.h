cea25d4dbcc5b089ac31acfde581650e0c5b4d23
Shah Sharvil <spshah16@asu.edu>
2018-07-03 14:01:17 -0700

    correction: GPIO3_LEV pin info

--- linux_drivers/debugfs_usage/debugfs_usage.h
+++ linux_drivers/debugfs_usage/debugfs_usage.h
@@ -15,7 +15,7 @@
 #define GPIO2 13
 #define GPIO2_LEV 34
 
-/*	14 and 26 correspond to linux pin_no
+/*	14 and 16 correspond to linux pin_no
  *	and level shifter pin resp. for GPIO3
  */
 #define GPIO3 14
